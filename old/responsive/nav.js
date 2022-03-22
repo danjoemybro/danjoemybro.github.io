b0bd4a674e7ff6cdf6edcc61c6ce1cdc27e47201
danjoemybro <danjoestubbs@gmail.com>
2022-03-22 11:41:19 +0000

    reorganise and new home page

---
+++ old/responsive/nav.js
@@ -0,0 +1,16 @@
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const icon = document.querySelector(".material-icons-round");
+
+const toggleMenu = (e) => {
+  let visibility = primaryNav.getAttribute("data-visible");
+  visibility = JSON.parse(visibility);
+
+  primaryNav.setAttribute("data-visible", !visibility);
+
+  icon.innerHTML = visibility ? "menu" : "close";
+
+  console.log(!visibility);
+};
+
+navToggle.addEventListener("click", toggleMenu);
