9c4d959570df788d6708e77da4cbbb1786cdec19
danjoemybro <danjoestubbs@gmail.com>
2022-03-22 18:46:20 +0000

    heareder rows

---
+++ js/modal-image.js
@@ -0,0 +1,46 @@
+// image ids to open
+const imageIds = [
+  "dan-and-vashti",
+  "c-01",
+  "c-02",
+  "c-03",
+  "c-04",
+  "c-05",
+  "c-06",
+  "c-07",
+  "c-08",
+];
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+imageIds.map((s, i) => {
+  var img = document.getElementById(s);
+
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  };
+});
+
+const closeModal = () => {
+  modal.style.display = "none";
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = closeModal;
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
